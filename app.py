1ea290523e902a4be0ed353ac573259786e216a2
Rob Spectre <rob.spectre@gmail.com>
2012-01-13 13:13:38 -0500

    Added client

--- app.py
+++ app.py
@@ -16,10 +16,12 @@ def karaoke():
 def mic():
     capability = TwilioCapability(os.environ.get('ACCOUNT_SID'),
             os.environ.get('AUTH_TOKEN'))
-    capability.allow_outgoing(os.environ.get('SPEAKER_CALLER_ID'))
+    capability.allow_client_outgoing(os.environ.get('SPEAKER_CALLER_ID'))
     token = capability.generate()
     return flask.render_template('client.html', token=token, name='Microphone')
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
+    if port == 5000:
+        app.debug = True
     app.run(host='0.0.0.0', port=port)
