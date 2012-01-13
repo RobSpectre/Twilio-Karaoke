c4f8e50be736a64f1605b31cdcca75375581dda4
Rob Spectre <rob.spectre@gmail.com>
2012-01-13 14:11:56 -0500

    Created karaoke room.

---
+++ app.py
@@ -0,0 +1,17 @@
+import flask
+from twilio import twiml
+import os
+
+app = flask.Flask(__name__)
+
+@app.route('/karaoke')
+def karaoke():
+    response = twiml.Response()
+    with response.dial() as dial:
+        dial.conference("PennApps Karaoke Party Extravamagasma")
+    return str(response)
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.debug = True
+    app.run(host='0.0.0.0', port=port)
