5d70aebbee3af18a986c08b975fd1e20ed039c1a
Rob Spectre <rob.spectre@gmail.com>
2012-01-13 13:10:06 -0500

    Added client

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import flask
 from twilio import twiml
+from twilio.util import TwilioCapability
 import os
 
 app = flask.Flask(__name__)
@@ -10,6 +11,14 @@ def karaoke():
     with response.dial() as dial:
         dial.conference('PennApps Karaoke Party')
     return str(response)
+
+@app.route('/mic')
+def mic()
+    capability = TwilioCapability(os.environ.get('ACCOUNT_SID'),
+            os.environ.get('AUTH_TOKEN'))
+    capability.allow_outgoing(os.environ.get('SPEAKER_CALLER_ID'))
+    token = capability.generate()
+    return flask.render_template('client.html', token=token, name='Microphone')
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
